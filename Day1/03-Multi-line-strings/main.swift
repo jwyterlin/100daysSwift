d4cc54672d4559b78ded5d1fc0125e57b1e14f56
Jhonathan Wyterlin <jhonathanwyterlin@gmail.com>
2025-12-21 21:45:27 +0100

    Day 1 - Multi-line strings

---
+++ Day1/03-Multi-line-strings/main.swift
@@ -0,0 +1,16 @@
+var multilineText = """
+This is a multi-line text
+It contains more than one line of content
+This is the third line
+This is the forth line
+"""
+
+print(multilineText)
+
+var multilineTextForCodeFormattingPurposesOnly = """
+This is a text without line breaks \
+It is multiline only for code formatting \
+But not for the end user
+"""
+
+print(multilineTextForCodeFormattingPurposesOnly)
