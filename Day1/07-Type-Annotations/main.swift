603508aca58b7aa2335579c450a180de8a8b8038
Jhonathan Wyterlin <jhonathanwyterlin@gmail.com>
2025-12-21 22:10:14 +0100

    Day 1 - Type Annotations

---
+++ Day1/07-Type-Annotations/main.swift
@@ -0,0 +1,9 @@
+let album: String = "Images and Words"
+let year: Int = 1992
+let height: Double = 1.72
+let dreamTheaterRocks: Bool = true
+
+print("The type of the 'album' is \(type(of: album))")
+print("The type of the 'year' is \(type(of: year))")
+print("The type of the 'height' is \(type(of: height))")
+print("The type of the 'dreamTheaterRocks' is \(type(of: dreamTheaterRocks))")
