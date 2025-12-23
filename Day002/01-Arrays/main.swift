00f68965a81d363a44f6eb99311e890f2fd5bf8e
Jhonathan Wyterlin <jhonathanwyterlin@gmail.com>
2025-12-23 22:01:15 +0100

    Day 2 - Arrays

---
+++ Day002/01-Arrays/main.swift
@@ -0,0 +1,15 @@
+let john = "John Lennon"
+let paul = "Paul McCartney"
+let george = "George Harrison"
+let ringo = "Ringo Starr"
+
+let beatles = [john, paul, george, ringo]
+
+// Using type annotations
+let names: [String] = ["John", "James", "Mike", "Jordan"]
+
+let numbers: [Int] = [14, 26, 75, 94]
+
+let measurements: [Double] = [2.14, 6.26, 83.952, 0.9232, 915.256]
+
+let occurrences: [Bool] = [true, false, false, true, true, false]
