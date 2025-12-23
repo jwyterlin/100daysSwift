1c0a6cd013079000ba1211cd5c86c32730f01dd3
Jhonathan Wyterlin <jhonathanwyterlin@gmail.com>
2025-12-23 22:13:04 +0100

    Day 2 - Sets

---
+++ Day002/02-Sets/main.swift
@@ -0,0 +1,5 @@
+// Creating a set from an array
+let colors = Set(["red", "green", "blue"])
+
+// Duplicated colors don't appear repeated in the set
+let otherColors = Set(["white", "yellow", "brown", "yellow", "cyan"])
